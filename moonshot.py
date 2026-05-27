7cce72fd44f0bd3bc0a9f6a324d8857af87830de
Akshant <66271383+deadbo8@users.noreply.github.com>
2026-05-27 06:38:26 +0000

    Update moonshot.py

--- moonshot.py
+++ moonshot.py
@@ -15,8 +15,8 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 INDEX_KEY = 'NSE_INDEX|Nifty 50'
 EXPIRY_DATE = '2026-06-09'
-MIN_IV = 0.23              # REMINDER: Change back to your strategy minimums for live scanning
-MIN_VEGA = 3.0           # REMINDER: Change back to your strategy minimums for live scanning
+MIN_IV = 0.20              # REMINDER: Change back to your strategy minimums for live scanning
+MIN_VEGA = 2.7           # REMINDER: Change back to your strategy minimums for live scanning
 MIN_STRIKE_DISTANCE = 1000  # REMINDER: Change back to your strategy minimums for live scanning
 # -----------------------------------------------
 
